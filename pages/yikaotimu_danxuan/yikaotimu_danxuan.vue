0205bdde7180e2fc183dc9b9f0c083eb72e38503
gaojie2 <3540159594@qq.com>
2021-04-13 11:16:12 +0800

    1.实现多选的静态页面

--- pages/yikaotimu_danxuan/yikaotimu_danxuan.vue
+++ pages/yikaotimu_danxuan/yikaotimu_danxuan.vue
@@ -18,7 +18,7 @@
 		</view>
 		<view class="yikaotimu_danxuan_content_content">
 			<view class="yikaotimu_danxuan_content_content1">
-				<view class="yikaotimu_danxuan_content_content1_select">
+				<!-- <view class="yikaotimu_danxuan_content_content1_select">
 					<view class="yikaotimu_danxuan_content_content1_select_left">A</view>
 					<view class="yikaotimu_danxuan_content_content1_select_right">嵇康</view>
 				</view>
@@ -33,11 +33,36 @@
 				<view class="yikaotimu_danxuan_content_content1_select">
 					<view class="yikaotimu_danxuan_content_content1_select_left">D</view>
 					<view class="yikaotimu_danxuan_content_content1_select_right">阮瑀</view>
-				</view>
+				</view> -->
+				
+				<radio-group @change="radioChange">
+					<label class="uni-list-cell uni-list-cell-pd" v-for="(item, index) in items" :key="item.value">
+						<view class="radio_select">
+							<radio class="radio_select1" :value="item.value" :checked="index === current" color="#5FB8A2" />
+							<view class="radio_select2">{{item.name}}</view>
+						</view>
+						
+					</label>
+				</radio-group>
+				
+				 <!-- 单个复选框 -->
+				<!-- <checkbox-group class="block" @change="checkboxChange">
+					<view class="cu-form-group">
+						<view class="title">复选选操作(checkbox)</view>
+						<checkbox :class="isChecked?'checked':''" :checked="isChecked?true:false" value="1"></checkbox>
+					</view>
+				</checkbox-group> -->
+				
+				<checkbox-group class="block" @change="changeCheckbox">
+					<view v-for="item in checkboxData" :key="item.value">                
+						<checkbox color="#5FB8A2" :value="String(item.value)" :checked="checkedArr.includes(String(item.value))" :class="{'checked':checkedArr.includes(String(item.value))}"></checkbox>
+						<text>{{item.label}}</text>
+					</view>
+				</checkbox-group>
 			</view>
 		</view>
 		<view class="yikaotimu_danxuan_next">
-			<view class="yikaotimu_danxuan_next_button"></view>
+			<view class="yikaotimu_danxuan_next_button">下一题</view>
 		</view>
 	</view>
 </template>
@@ -46,11 +71,64 @@
 	export default{
 		data(){
 			return{
-				
+				isChecked:false,
+				checkboxData:[
+					{'value':0,'label':'嵇康'},
+					{'value':1,'label':'曹植'},
+					{'value':2,'label':'山涛'},
+					{'value':3,'label':'阮瑀'},
+				],
+				checkedArr:[], //复选框选中的值
+				allChecked:false, //是否全选
+				items: [{
+							value: 'USA',
+							name: '嵇康'
+						},
+						{
+							value: 'CHN',
+							name: '曹植',
+							checked: 'true'
+						},
+						{
+							value: 'BRA',
+							name: '山涛'
+						},
+						{
+							value: 'BRA3',
+							name: '阮瑀'
+						}
+					],
+					current: 0
 			}
 		},
 		methods:{
-			
+			// 单个复选框事件
+			checkboxChange(e) {
+				let values = e.detail.value;
+				if(values[0]==1){
+					this.isChecked=true;
+				}else{
+					this.isChecked=false;
+				}
+			},
+			// 多选复选框改变事件
+			changeCheckbox(e){
+				this.checkedArr = e.detail.value;
+				// 如果选择的数组中有值，并且长度等于列表的长度，就是全选
+				if(this.checkedArr.length>0 && this.checkedArr.length==this.checkboxData.length){
+					this.allChecked=true;
+				}else{
+					this.allChecked=false;
+				}
+			},
+			radioChange: function(evt) {
+				for (let i = 0; i < this.items.length; i++) {
+					if (this.items[i].value === evt.target.value) {
+						this.current = i;
+						break;
+					}
+				}
+			}
 		},
 	}
 </script>
@@ -164,6 +242,44 @@
 					margin-left:20rpx;
 				}
 			}
+			.radio_select{
+				width:100%;
+				height:50rpx;
+				margin-top:10rpx;
+				// border:1px solid blue;
+				.radio_select1{
+					float:left;
+					width:50rpx;
+					height:50rpx;
+					margin-left:20rpx;
+					// border:1px solid blue;
+				}
+				.radio_select2{
+					float:left;
+					width:300rpx;
+					height:50rpx;
+					margin-left:20rpx;
+					// border:1px solid blue;
+				}
+			}
+		}
+	}
+	.yikaotimu_danxuan_next{
+		width:100%;
+		height:80rpx;
+		// border:1px solid orange;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		.yikaotimu_danxuan_next_button{
+			width:140rpx;
+			height:50rpx;
+			// border:1px solid pink;
+			line-height: 50rpx;
+			text-align: center;
+			// border-radius: 10rpx;
+			background-color: #4CB5F6;
+			color:#FFFFFF;
 		}
 	}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/yikaotimu_danxuan/yikaotimu_danxuan.js
+++ unpackage/dist/dev/mp-weixin/pages/yikaotimu_danxuan/yikaotimu_danxuan.js
@@ -97,6 +97,28 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
+  var l0 = _vm.__map(_vm.checkboxData, function(item, __i0__) {
+    var $orig = _vm.__get_orig(item)
+
+    var g0 = _vm.checkedArr.includes(String(item.value))
+    var m0 = String(item.value)
+    var g1 = _vm.checkedArr.includes(String(item.value))
+    return {
+      $orig: $orig,
+      g0: g0,
+      m0: m0,
+      g1: g1
+    }
+  })
+
+  _vm.$mp.data = Object.assign(
+    {},
+    {
+      $root: {
+        l0: l0
+      }
+    }
+  )
 }
 var recyclableRender = false
 var staticRenderFns = []
@@ -174,14 +196,93 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 var _default =
 {
   data: function data() {
-    return {};
+    return {
+      isChecked: false,
+      checkboxData: [
+      { 'value': 0, 'label': '嵇康' },
+      { 'value': 1, 'label': '曹植' },
+      { 'value': 2, 'label': '山涛' },
+      { 'value': 3, 'label': '阮瑀' }],
 
+      checkedArr: [], //复选框选中的值
+      allChecked: false, //是否全选
+      items: [{
+        value: 'USA',
+        name: '嵇康' },
+
+      {
+        value: 'CHN',
+        name: '曹植',
+        checked: 'true' },
+
+      {
+        value: 'BRA',
+        name: '山涛' },
+
+      {
+        value: 'BRA3',
+        name: '阮瑀' }],
+
+
+      current: 0 };
 
   },
-  methods: {} };exports.default = _default;
+  methods: {
+    // 单个复选框事件
+    checkboxChange: function checkboxChange(e) {
+      var values = e.detail.value;
+      if (values[0] == 1) {
+        this.isChecked = true;
+      } else {
+        this.isChecked = false;
+      }
+    },
+    // 多选复选框改变事件
+    changeCheckbox: function changeCheckbox(e) {
+      this.checkedArr = e.detail.value;
+      // 如果选择的数组中有值，并且长度等于列表的长度，就是全选
+      if (this.checkedArr.length > 0 && this.checkedArr.length == this.checkboxData.length) {
+        this.allChecked = true;
+      } else {
+        this.allChecked = false;
+      }
+    },
+    radioChange: function radioChange(evt) {
+      for (var i = 0; i < this.items.length; i++) {
+        if (this.items[i].value === evt.target.value) {
+          this.current = i;
+          break;
+        }
+      }
+    } } };exports.default = _default;
 
 /***/ }),
 
