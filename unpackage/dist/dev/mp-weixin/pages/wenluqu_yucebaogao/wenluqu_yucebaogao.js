418c8ab5d26c2a21c17eceee03f573a6011fde37
gaojie2 <3540159594@qq.com>
2021-04-15 16:27:11 +0800

    1.使用图片转base64解决预测报告背景图过大的问题 2.使用windowHeight解决背景在手机页面，高度没有填满的问题

--- unpackage/dist/dev/mp-weixin/pages/wenluqu_yucebaogao/wenluqu_yucebaogao.js
+++ unpackage/dist/dev/mp-weixin/pages/wenluqu_yucebaogao/wenluqu_yucebaogao.js
@@ -127,8 +127,10 @@ __webpack_require__.r(__webpack_exports__);
   !*** ./node_modules/babel-loader/lib!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/webpack-preprocess-loader??ref--12-1!./node_modules/@dcloudio/webpack-uni-mp-loader/lib/script.js!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/vue-loader/lib??vue-loader-options!./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js!D:/HbuilderX/hexiangu_web/pages/wenluqu_yucebaogao/wenluqu_yucebaogao.vue?vue&type=script&lang=js& ***!
   \*****************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
+"use strict";
+/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0; //
 //
 //
 //
@@ -160,7 +162,32 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
+var _default =
+{
+  data: function data() {
+    return {
+      //适配手机高度
+      phoneHeight: 0 };
+
+  },
+  onLoad: function onLoad() {
+    this.getWindowHeight();
+  },
+  methods: {
+    //获取窗口高度，适配手机
+    getWindowHeight: function getWindowHeight() {var _this = this;
+      uni.getSystemInfo({
+        success: function success(res) {
+          // console.log(res);
+          // console.log("手机可用高度:"+res.windowHeight*2+"rpx");
+          _this.phoneHeight = res.windowHeight;
+          // console.log(res.windowHeight);
+          // console.log(this.phoneHeight);
+          // this.$store.commit('set_window_height',res.windowHeight*2);
+        } });
+
+    } } };exports.default = _default;
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["default"]))
 
 /***/ }),
 
