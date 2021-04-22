cf7e0240f751a3dddd07966a81cfc58fa23c4e2a
gaojie2 <3540159594@qq.com>
2021-04-22 19:45:25 +0800

    1.完成艺考题库，选择某一题目的上一题，下一题

--- pages/yikaotimu_danxuan/yikaotimu_danxuan.vue
+++ pages/yikaotimu_danxuan/yikaotimu_danxuan.vue
@@ -5,8 +5,8 @@
 		</view>
 		<view class="yikaotimu_danxuan_tihao">
 			<view class="yikaotimu_danxuan_tihao_title">
-				<view class="yikaotimu_danxuan_tihao_title1">1</view>
-				<view class="yikaotimu_danxuan_tihao_title2">/50题</view>
+				<view class="yikaotimu_danxuan_tihao_title1">{{currentId}}</view>
+				<view class="yikaotimu_danxuan_tihao_title2">/{{nums}}题</view>
 			</view>
 			<view class="yikaotimu_danxuan_tihao_jiaojuan">立即交卷</view>
 			<view class="yikaotimu_danxuan_tihao_img">
@@ -14,7 +14,7 @@
 			</view>
 		</view>
 		<view class="yikaotimu_danxuan_content_title">
-			<view class="yikaotimu_danxuan_content_title1">【单选】魏晋时期，建安七子是当时文学的代表人物，下列属于建安七子的是（）。</view>
+			<view class="yikaotimu_danxuan_content_title1">【单选】{{content}}（）。</view>
 		</view>
 		<view class="yikaotimu_danxuan_content_content">
 			<view class="yikaotimu_danxuan_content_content1">
@@ -36,10 +36,10 @@
 				</view> -->
 				
 				<radio-group @change="radioChange">
-					<label class="uni-list-cell uni-list-cell-pd" v-for="(item, index) in items" :key="item.value">
+					<label class="uni-list-cell uni-list-cell-pd" v-for="(item, index) in items" :key="item.quId">
 						<view class="radio_select">
-							<radio class="radio_select1" :value="item.value" :checked="index === current" color="#5FB8A2" />
-							<view class="radio_select2">{{item.name}}</view>
+							<radio class="radio_select1" :quId="item.quId" :checked="index === current" color="#5FB8A2" />
+							<view class="radio_select2">{{item.content}}</view>
 						</view>
 						
 					</label>
@@ -62,12 +62,14 @@
 			</view>
 		</view>
 		<view class="yikaotimu_danxuan_next">
-			<view class="yikaotimu_danxuan_next_button">下一题</view>
+			<view class="yikaotimu_danxuan_front_button" @click="click_front" :class="isshowFront?'showFront':''">上一题</view>
+			<view class="yikaotimu_danxuan_next_button" @click="click_next" :class="isshowNext?'showNext':''">下一题</view>
 		</view>
 	</view>
 </template>
 
 <script>
+	import {getyikaoTikuList_one_one} from '../../api/api.js'
 	export default{
 		data(){
 			return{
@@ -80,28 +82,83 @@
 				],
 				checkedArr:[], //复选框选中的值
 				allChecked:false, //是否全选
-				items: [{
-							value: 'USA',
-							name: '嵇康'
-						},
-						{
-							value: 'CHN',
-							name: '曹植',
-							checked: 'true'
-						},
-						{
-							value: 'BRA',
-							name: '山涛'
-						},
-						{
-							value: 'BRA3',
-							name: '阮瑀'
-						}
+				items: [
+					 //    {
+						// 	value: 'USA',
+						// 	content: '嵇康'
+						// },
+						// {
+						// 	value: 'CHN',
+						// 	content: '曹植',
+						// 	checked: 'true'
+						// },
+						// {
+						// 	value: 'BRA',
+						// 	content: '山涛'
+						// },
+						// {
+						// 	value: 'BRA3',
+						// 	content: '阮瑀'
+						// }
 					],
-					current: 0
+					//目前选项的index
+					current: 0,
+					//题目的页码
+					currentId:1,
+					//题目总数
+					nums:0,
+					//单选题标题
+					content:'',
+					//显示上一题
+					isshowFront:true,
+					//显示下一题
+					isshowNext:true
+			}
+		},
+		onLoad(id) {
+			console.log(id.ids1);
+			// this.currentId=id.ids1;
+			if(id.ids1==undefined||id.ids1=="1"){
+				let id=1;
+				this.isshowFront=false;
+				getyikaoTikuList_one_one(id).then((res)=>{
+					console.log(res.data.data);
+					this.content=res.data.data[0].content;
+					this.items=res.data.data[0].answers;
+				});
+			}else{
+				getyikaoTikuList_one_one(id.ids1).then((res)=>{
+					console.log(res);
+					this.currentId=id.ids1;
+					this.content=res.data.data[0].content;
+					this.items=res.data.data[0].answers;
+				});
+			}
+			//通过试卷id获取题目的id
+			// this.currentId=id.ids;
+			let arrs=uni.getStorageSync('yikaoTikuList_one1');
+			this.nums=arrs.length;
+			//最后一题目，不显示下一页
+			if(id.ids1==arrs.length){
+				this.isshowNext=false;
 			}
 		},
 		methods:{
+			//点击，跳转到上一页
+			click_front(){
+				let id=parseInt(this.currentId)-1
+				uni.reLaunch({
+					url:'../yikaotimu_danxuan/yikaotimu_danxuan?ids1='+id
+				})
+			},
+			//点击，跳转到下一页
+			click_next(){
+				// console.log(111);
+				let id=parseInt(this.currentId)+1
+				uni.reLaunch({
+					url:'../yikaotimu_danxuan/yikaotimu_danxuan?ids1='+id
+				})
+			},
 			// 单个复选框事件
 			checkboxChange(e) {
 				let values = e.detail.value;
@@ -271,6 +328,21 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
+		.yikaotimu_danxuan_front_button{
+			width:140rpx;
+			height:50rpx;
+			// border:1px solid pink;
+			line-height: 50rpx;
+			text-align: center;
+			margin-right:50rpx;
+			// border-radius: 10rpx;
+			background-color: orange;
+			color:#FFFFFF;
+			display: none;
+		}
+		.showFront{
+			display: block;
+		}
 		.yikaotimu_danxuan_next_button{
 			width:140rpx;
 			height:50rpx;
@@ -280,6 +352,10 @@
 			// border-radius: 10rpx;
 			background-color: #4CB5F6;
 			color:#FFFFFF;
+			display: none;
+		}
+		.showNext{
+			display: block;
 		}
 	}
 </style>
