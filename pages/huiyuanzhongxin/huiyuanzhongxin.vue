68cf5c9e831bc3f7db2ba2fd4fe5c2f4326c7b00
hhh <xxx@qq.com>
2021-05-24 11:56:30 +0800

    合并

--- pages/huiyuanzhongxin/huiyuanzhongxin.vue
+++ pages/huiyuanzhongxin/huiyuanzhongxin.vue
@@ -2,8 +2,7 @@
 	<view class="box">
 		<!-- 轮播图区域 -->
 		<view style="margin: 30rpx 0rpx;">
-			<swiper :current="currentIndex" :duration="500" @change="swierChange" previous-margin="60rpx"
-				next-margin="40rpx">
+			<swiper :current="currentIndex" :duration="500" @change="swierChange" previous-margin="60rpx" next-margin="40rpx" class="swiper-box">
 				<swiper-item>
 					<view class="professional">
 						<view class="user-info">
@@ -13,14 +12,10 @@
 									百年老字号
 									<image src="../../static/img/my/jiangpaixunzhang1.png"></image>
 								</view>
-								<view class="deadline">
-									会员有效期至：2021-06-6
-								</view>
+								<view class="deadline">会员有效期至：2021-06-6</view>
 							</view>
 						</view>
-						<view class="renew">
-							立即续费
-						</view>
+						<view class="renew">立即续费</view>
 					</view>
 				</swiper-item>
 				<swiper-item>
@@ -32,183 +27,230 @@
 									百年老字号
 									<image src="../../static/img/my/jiangpaixunzhang.png"></image>
 								</view>
-								<view class="deadline">
-									会员有效期至：2021-06-6
-								</view>
+								<view class="deadline">会员有效期至：2021-06-6</view>
 							</view>
 						</view>
 						<view class="handle">
-							<view class="renew">
-								立即续费
-							</view>
-							<view class="renew" style="margin-left: 40rpx;">
-								升级会员
-							</view>
+							<view class="renew">立即续费</view>
+							<view class="renew" style="margin-left: 40rpx;">升级会员</view>
 						</view>
 					</view>
 				</swiper-item>
 			</swiper>
 		</view>
 		<!-- 特权区域 -->
-		<view class="privilege">
+		<view class="privilege" >
 			<!-- 专业版特权 -->
-			<view class="pro">
-				<view class="title">
-					专业版特权
-				</view>
+			<view class="pro" v-show="currentIndex===0">
+				<view class="title">专业版特权</view>
 				<view class="items">
 					<view class="item">
-						<view class="item-img">
-							<image src="../../static/img/my/01_会员信息管理.png" mode=""></image>
-						</view>
+						<view class="item-img"><image src="../../static/img/my/01_会员信息管理.png" mode=""></image></view>
 						<text>优质建议</text>
 					</view>
-
+					<view class="item">
+						<view class="item-img"><image src="../../static/img/my/c1-特权.png" mode=""></image></view>
+						<text>专属铭牌</text>
+					</view>
+					<view class="item">
+						<view class="item-img"><image src="../../static/img/my/生日礼物.png" mode=""></image></view>
+						<text>资源优享</text>
+					</view>
+					<view class="item">
+						<view class="item-img"><image src="../../static/img/my/表情.png" mode=""></image></view>
+						<text>贴心交流</text>
+					</view>
 				</view>
+				<!-- 支付 -->
+				<view class="cost">支付99元</view>
+			</view>
+			<!-- 会员版特权 -->
+			<view class="pro" v-show="currentIndex==1">
+				<view class="title">会员版特权</view>
+				<view class="items">
+					<view class="item">
+						<view class="item-img"><image src="../../static/img/my/01_会员信息管理.png" mode=""></image></view>
+						<text>优质建议</text>
+					</view>
+					<view class="item">
+						<view class="item-img"><image src="../../static/img/my/c1-特权.png" mode=""></image></view>
+						<text>专属铭牌</text>
+					</view>
+				</view>
+				<!-- 支付 -->
+				<view class="cost">支付9.9元</view>
 			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {};
-		},
-		methods: {}
-	};
+export default {
+	data() {
+		return {
+			// 索引代表会员类型 0 专业 1 非专业
+			currentIndex:0,
+		};
+	},
+	methods: {
+		// 轮播图转动时
+		swierChange(obj){
+			this.currentIndex=obj.detail.current
+			console.log(this.currentIndex)
+		}
+	}
+};
 </script>
 
 <style>
-	.box {
-		width: 100%;
-	}
+.box {
+	width: 100%;
+	padding-top: 30rpx;
+}
+.swiper-box {
+	height: 320rpx;
+}
 
-	uni-swiper {
-		height: 320rpx;
-	}
+.professional,
+.members {
+	width: 95%;
+	height: 320rpx;
+	background: url(../../static/img/my/huiyuan1.png) no-repeat 0px 0px;
+	background-size: 100% 320rpx;
+	border-radius: 16rpx;
+	overflow: hidden;
+}
 
-	.professional,
-	.members {
-		width: 95%;
-		height: 320rpx;
-		background: url(../../static/img/my/huiyuan1.png) no-repeat 0px 0px;
-		background-size: 100% 320rpx;
-		border-radius: 16rpx;
-		overflow: hidden;
-	}
+.members {
+	background: url(../../static/img/my/huiyuan2.png) no-repeat 0px 0px;
+}
 
-	.members {
-		background: url(../../static/img/my/huiyuan2.png) no-repeat 0px 0px;
-	}
+.user-info {
+	display: flex;
+	width: 100%;
+	height: 100rpx;
+	align-items: center;
+	margin: 40rpx 0rpx 60rpx;
+	letter-spacing: 2rpx;
+}
 
-	.user-info {
-		display: flex;
-		width: 100%;
-		height: 100rpx;
-		align-items: center;
-		margin: 40rpx 0rpx 60rpx;
-		letter-spacing: 2rpx;
-	}
+.user-info .img {
+	width: 84rpx;
+	height: 84rpx;
+	margin: 0rpx 40rpx;
+}
 
-	.user-info .img {
-		width: 84rpx;
-		height: 84rpx;
-		margin: 0rpx 40rpx;
-	}
+.detail {
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	justify-content: space-between;
+	height: 100rpx;
+}
 
-	.detail {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		justify-content: space-between;
-		height: 100rpx;
-	}
+.name {
+	font-size: 16px;
+	font-weight: 400;
+	line-height: 44rpx;
+	color: #ffffff;
+}
 
-	.name {
-		font-size: 16px;
-		font-weight: 400;
-		line-height: 44rpx;
-		color: #FFFFFF;
-	}
+.name image {
+	width: 36rpx;
+	height: 36rpx;
+	margin: 0rpx 0rpx 4rpx 10rpx;
+	vertical-align: middle;
+}
 
-	.name image {
-		width: 36rpx;
-		height: 36rpx;
-		margin: 0rpx 0rpx 4rpx 10rpx;
-		vertical-align: middle;
-	}
+.deadline {
+	font-size: 12px;
+	font-weight: 400;
+	line-height: 34rpx;
+	color: #ffffff;
+}
 
-	.deadline {
-		font-size: 12px;
-		font-weight: 400;
-		line-height: 34rpx;
-		color: #FFFFFF;
-	}
+.handle {
+	display: flex;
+	width: 100%;
+}
 
-	.handle {
-		display: flex;
-		width: 100%;
-	}
+.renew {
+	width: 152rpx;
+	height: 60rpx;
+	margin-left: 164rpx;
+	background: #ffffff;
+	text-align: center;
+	line-height: 60rpx;
+	font-size: 24rpx;
+	color: #273253;
+	border-radius: 30rpx;
+}
 
-	.renew {
-		width: 152rpx;
-		height: 60rpx;
-		margin-left: 164rpx;
-		background: #FFFFFF;
-		text-align: center;
-		line-height: 60rpx;
-		font-size: 24rpx;
-		color: #273253;
-		border-radius: 30rpx;
-	}
+.privilege {
+	padding: 0rpx 60rpx;
+}
 
-	.privilege {
-		padding: 0rpx 60rpx;
-	}
+.pro .title {
+	width: 100%;
+	height: 44rpx;
+	font-size: 16px;
+	font-weight: 400;
+	line-height: 44rpx;
+	color: #273253;
+}
+.pro{
+	position: relative;
+}
+.items {
+	display: flex;
+	flex-wrap: wrap;
+}
 
-	.pro .title {
-		width: 100%;
-		height: 44rpx;
-		font-size: 16px;
-		font-weight: 400;
-		line-height: 44rpx;
-		color: #273253;
-	}
+.item {
+	display: flex;
+	width: 100rpx;
+	flex-direction: column;
+	align-items: center;
+	margin: 60rpx 100rpx 0rpx 0rpx ;
+}
 
-	.items {
-		display: flex;
-	}
+.item-img {
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-	.item {
-		display: flex;
-		width: 100rpx;
-		flex-direction: column;
-		align-items: center;
-	}
+	width: 100rpx;
+	height: 100rpx;
+	background: #ffffff;
+	border: 1px solid #ed5c4d;
+	border-radius: 50%;
+}
 
-	.item-img {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-
-		width: 100rpx;
-		height: 100rpx;
-		background: #FFFFFF;
-		border: 1px solid #ED5C4D;
-		border-radius: 50%;
-	}
-
-	.item-img image {
-		width: 60rpx;
-		height: 60rpx;
-	}
-	.item text{
-		width: 52px;
-		font-size: 24rpx;
-		font-weight: 400;
-		line-height: 40rpx;
-		margin-top: 20rpx;
-		color: #273253;
-	}
+.item-img image {
+	width: 60rpx;
+	height: 60rpx;
+}
+.item text {
+	width: 52px;
+	font-size: 24rpx;
+	font-weight: 400;
+	line-height: 40rpx;
+	margin-top: 20rpx;
+	color: #273253;
+}
+.cost {
+	position: absolute;
+	top: 300rpx;
+	left: 0rpx;
+	width: 100%;
+	height: 88rpx;
+	background: #ed5c4d;
+	margin-top: 300rpx;
+	line-height: 88rpx;
+	text-align: center;
+	border-radius: 24px;
+	font-size: 18px;
+	font-weight: 400;
+	color: #ffffff;
+}
 </style>
--- pages/test_search/test_search.vue
+++ pages/test_search/test_search.vue
@@ -1,206 +1,208 @@
 <template>
-<view>
-	<!-- <uniNavBar></uniNavBar> -->
-<!-- 搜索框 -->
-	<view class="search">
-		<view class="search1">
-			<text class="iconfont icon-sousuo position-absolute text-muted"></text>
-			<input class="searchInput" v-model="inputValue" @confirm="search" placeholder="请输入艺考动态标题..." type="text" />
+	<view class="box">
+		<!-- 搜索区域 -->
+		<view class="search-box">
+			<text class="iconfont icon-sousuo sousuo"></text>
+			<input style="padding-left: 80rpx;" :placeholder="placeholders" placeholder-style="font-size: 32rpx; color:#A9AFB8;"
+			 v-model="inputValue"
+			 @confirm="search"
+			 />
+			<text class="quxiao" @click="qingkongSearchvalue">取消</text>
 		</view>
-		<view  class="searchQuxiao" @click="qingkongSearchvalue">取消</view>
-	</view>
-<!-- 搜索框 -->
-
-<!-- 搜索历史 -->
-	<view class="searchHistory">
-		<view class="searchHistory1" :class="isShow?'isShows':'isNotShows'">
-			<view class="lishiSearch_title">历史搜索</view>
-			<view  @click="empty" class="lishiSearch_title1">
-				<image src="../../static/img/delete_history.png"></image>
+		
+		<!-- 热搜区域 -->
+		<view class="hot">
+		<view class="title">
+			热搜
+		</view>
+		<view class="items">
+			<view class="item" @click="click_item_search('春江花月夜')">
+				春江花月夜
 			</view>
 		</view>
-		<view class="searchHistoryItem" :class="isShow?'isShows':'isNotShows'">
-			<view v-for="(item, index) in searchHistoryList" :key="index" class="searchHistoryItem1">
-				<text class="searchHistoryItem_item" :class="isShow?'isShows':'isNotShows'" @click="click_item_search(item)">{{ item }}</text>
+		</view>
+		<!-- 搜索历史 -->
+		<view class="history" v-show="isShow">
+			<view class="title">
+				搜索历史
+			</view>
+			<view class="items">
+				<view class="item" v-for="(item,index) in searchHistoryList" :key='index' @click="click_item_search(item)">
+					{{item}}
+				</view>
+			</view>
+			<view class="clear"  @click="empty">
+				<image src="../../static/img/delete_history.png">
+				<text >
+					清空搜索历史
+				</text>
 			</view>
 		</view>
-		<view class="content_contents" :style="{height:phoneHeight*0.65+'px;'}">
-			<view v-for="(item,index) in yikaiDongtaiList" :key="index">
-				<view class="content_content"  @click="yikaoDongtai(item.id)" :style="{height:phoneHeight*0.15+'px;'}">
-					<view class="content_content1">
-						<view class="content_content1_left">
-							<view class="content_content1_left_top">{{item.aedTitle}}</view>
-							<view class="content_content1_left_bottom">
-								<view class="content_content1_left_bottom1">{{item.aedPublisheder}}</view>
-								<view class="content_content1_left_bottom2">{{item.aedCreatetime}}</view>
+		
+		<!-- 搜索列表 -->
+		<view class="content-box">
+			<view class="dynamic-contents" >
+				<view v-for="(item, index) in yikaiDongtaiList" :key="index">
+					<view class="dynamic-content" @click="yikaoDongtai(item.id)">
+						<view class="dynamic-img"><image :src="item.aedMinimg"></image></view>
+						<view class="dynamic-detail">
+							<view class="dynamic-title">{{ item.aedTitle }}</view>
+							<view class="dynamic-pub">
+								<text>{{ item.aedPublisheder }}</text>
+								<text>{{ item.aedCreatetime }}</text>
 							</view>
-						</view>
-						<view class="content_content1_right">
-							<image :src="item.aedMinimg"></image>
 						</view>
 					</view>
 				</view>
-			</view>			
+			</view>
 		</view>
 	</view>
-<!-- 搜索历史 -->
-	
-</view>
 </template>
 
 <script>
-import {getmoreList1} from '../../api/api.js';
-// import uniNavBar from '../../components/uni-nav-bar/uni-nav-bar.vue';
+import { getmoreList1 } from '../../api/api.js';
 export default {
-// components:{
-// 	uniNavBar
-// },
-data() {
-	return {
-		inputValue: '',
-		searchHistoryList: [] ,//搜索出来的内容，
-		yikaiDongtaiList:[],
-		//是否显示历史搜索
-		isShow:true,
-		//适配手机高度
-		phoneHeight:0
-	};
-},
-onLoad() {
-	//获取窗口高度，适配手机
-	this.getWindowHeight();
-},
-methods: {
-	//获取窗口高度，适配手机
-	getWindowHeight(){
-		uni.getSystemInfo({
-			success:(res)=>{
-				// console.log(res);
-				// console.log("手机可用高度:"+res.windowHeight*2+"rpx");
-				this.phoneHeight=res.windowHeight;
-				// console.log(res.windowHeight);
-				// console.log(this.phoneHeight);
-				// this.$store.commit('set_window_height',res.windowHeight*2);
-			}
-		})
+	data() {
+		return {
+			inputValue: '',
+			searchHistoryList: [], //搜索出来的内容，
+			yikaiDongtaiList: [],
+			//是否显示历史搜索
+			isShow: true,
+			//适配手机高度
+			phoneHeight: 0,
+			// 提示信息
+			placeholders: '搜索'
+		};
 	},
-	//点击查看某一篇的艺考动态信息
-	yikaoDongtai(ids){
-		// console.log(ids);
-		
-		uni.navigateTo({
-			url:'../yikaoxiangqing/yikaoxiangqing?id='+ids
-		})
+	onLoad() {
+		//获取窗口高度，适配手机
+		this.getWindowHeight();
 	},
-	//点击历史记录的某一项搜索
-	click_item_search(item){
-		console.log(item);
-		this.isShow=false;
-		let aedTitle=item;
-		let currentPage=0;
-		let pageSize=10;
-		let sousuoTyoe=1;
-		getmoreList1(aedTitle,sousuoTyoe,currentPage,pageSize).then((res)=>{
-			// this.isSearch=false;
-			if(res.data.msg=='暂无数据,请期待哦!'){
-				uni.showModal({
-					title:'暂无数据,请期待哦!'
-				})
-			}else{
-				// console.log(res.data.artexamdynamicList);
-				this.yikaiDongtaiList=res.data.artexamdynamicList;
-				// console.log(this.yikaiDongtaiList);
-			}
-			// console.log(res.data.artexamdynamicList);
-			
-			
-		})
+	methods: {
+		//获取窗口高度，适配手机
+		getWindowHeight() {
+			uni.getSystemInfo({
+				success: res => {
+					// console.log(res);
+					// console.log("手机可用高度:"+res.windowHeight*2+"rpx");
+					this.phoneHeight = res.windowHeight;
+				}
+			});
+		},
+		//点击查看某一篇的艺考动态信息
+		yikaoDongtai(ids) {
+			// console.log(ids);
 
-	},
-	//取消,清空输入框的数据
-	qingkongSearchvalue(){
-		this.inputValue="";
-		// console.log(1111);
-		uni.reLaunch({
-			url:'../shouye/shouye'
-		})
-	},
-	search() {
-		if (this.inputValue == '') {
-			uni.showModal({
-			title: '搜索内容不能为空'
-		  });
-		  //解决ios鼠标无法弹回的问题
-		  uni.hideKeyboard();     //添加这一行代码即可
-		} else {
-			//如果历史记录缓存没有，就包含进去
-			if (!this.searchHistoryList.includes(this.inputValue)) {
-				this.searchHistoryList.unshift(this.inputValue);
-				uni.setStorage({
-					key: 'searchList',
-					data: JSON.stringify(this.searchHistoryList)
-			    });
-				this.isShow=false;
-				let aedTitle=this.inputValue;
-				let currentPage=0;
-				let pageSize=10;
-				let sousuoTyoe=1;
-				getmoreList1(aedTitle,sousuoTyoe,currentPage,pageSize).then((res)=>{
-					console.log(res.data.msg);
-					// this.isSearch=false;
-					if(res.data.msg=='暂无数据,请期待哦!'){
-						uni.showModal({
-							title:'暂无数据,请期待哦!'
-						})
-					}else{
-						// console.log(res.data.artexamdynamicList);
-						this.yikaiDongtaiList=res.data.artexamdynamicList;
-						// console.log(this.yikaiDongtaiList);
-					}
-					
-					//解决ios鼠标无法弹回的问题
-					uni.hideKeyboard();     //添加这一行代码即可
-				})
-				
-		} else {
-		//有搜索记录，删除之前的旧记录，将新搜索值重新push到数组首位
-			let i = this.searchHistoryList.indexOf(this.inputValue);
-			this.searchHistoryList.splice(i, 1);
-			this.searchHistoryList.unshift(this.inputValue);
-			uni.showToast({
-				title: '不能重复添加'
+			uni.navigateTo({
+				url: '../yikaoxiangqing/yikaoxiangqing?id=' + ids
 			});
-			uni.setStorage({
-				key: 'searchList',
-				data: JSON.stringify(this.searchHistoryList)
-			});
-			this.isShow=false;
-			let aedTitle=this.inputValue;
-			let currentPage=0;
-			let pageSize=10;
-			let sousuoTyoe=1;
-			getmoreList1(aedTitle,sousuoTyoe,currentPage,pageSize).then((res)=>{
+		},
+		//点击历史记录的某一项搜索
+		click_item_search(item) {
+			console.log(item);
+			this.isShow = false;
+			let aedTitle = item;
+			let currentPage = 0;
+			let pageSize = 10;
+			let sousuoTyoe = 1;
+			getmoreList1(aedTitle, sousuoTyoe, currentPage, pageSize).then(res => {
 				// this.isSearch=false;
-				console.log(res.data.artexamdynamicList);
-				this.yikaiDongtaiList=res.data.artexamdynamicList;
+				if (res.data.msg == '暂无数据,请期待哦!') {
+					uni.showModal({
+						title: '暂无数据,请期待哦!'
+					});
+				} else {
+					// console.log(res.data.artexamdynamicList);
+					this.yikaiDongtaiList = res.data.artexamdynamicList;
+					// console.log(this.yikaiDongtaiList);
+				}
+				// console.log(res.data.artexamdynamicList);
+			});
+		},
+		//取消,清空输入框的数据
+		qingkongSearchvalue() {
+			this.inputValue = '';
+			// console.log(1111);
+			uni.reLaunch({
+				url: '../shouye/shouye'
+			});
+		},
+		search() {
+			if (this.inputValue == '') {
+				uni.showModal({
+					title: '搜索内容不能为空'
+				});
 				//解决ios鼠标无法弹回的问题
-				uni.hideKeyboard();     //添加这一行代码即可
-			})
-		}
-	}
-},
-	//清空历史记录
-	empty() {
-		uni.showToast({
-			title: '已清空'
-		});
-		uni.removeStorage({
-			key: 'searchList'
-		});
+				uni.hideKeyboard(); //添加这一行代码即可
+			} else {
+				//如果历史记录缓存没有，就包含进去
+				if (!this.searchHistoryList.includes(this.inputValue)) {
+					this.searchHistoryList.unshift(this.inputValue);
+					uni.setStorage({
+						key: 'searchList',
+						data: JSON.stringify(this.searchHistoryList)
+					});
+					this.isShow = false;
+					let aedTitle = this.inputValue;
+					let currentPage = 0;
+					let pageSize = 10;
+					let sousuoTyoe = 1;
+					getmoreList1(aedTitle, sousuoTyoe, currentPage, pageSize).then(res => {
+						console.log(res.data.msg);
+						// this.isSearch=false;
+						if (res.data.msg == '暂无数据,请期待哦!') {
+							uni.showModal({
+								title: '暂无数据,请期待哦!'
+							});
+						} else {
+							// console.log(res.data.artexamdynamicList);
+							this.yikaiDongtaiList = res.data.artexamdynamicList;
+							// console.log(this.yikaiDongtaiList);
+						}
 
-		this.searchHistoryList = [];
-	}
-},
+						//解决ios鼠标无法弹回的问题
+						uni.hideKeyboard(); //添加这一行代码即可
+					});
+				} else {
+					//有搜索记录，删除之前的旧记录，将新搜索值重新push到数组首位
+					let i = this.searchHistoryList.indexOf(this.inputValue);
+					this.searchHistoryList.splice(i, 1);
+					this.searchHistoryList.unshift(this.inputValue);
+					uni.showToast({
+						title: '不能重复添加'
+					});
+					uni.setStorage({
+						key: 'searchList',
+						data: JSON.stringify(this.searchHistoryList)
+					});
+					this.isShow = false;
+					let aedTitle = this.inputValue;
+					let currentPage = 0;
+					let pageSize = 10;
+					let sousuoTyoe = 1;
+					getmoreList1(aedTitle, sousuoTyoe, currentPage, pageSize).then(res => {
+						// this.isSearch=false;
+						console.log(res.data.artexamdynamicList);
+						this.yikaiDongtaiList = res.data.artexamdynamicList;
+						//解决ios鼠标无法弹回的问题
+						uni.hideKeyboard(); //添加这一行代码即可
+					});
+				}
+			}
+		},
+		//清空历史记录
+		empty() {
+			uni.showToast({
+				title: '已清空'
+			});
+			uni.removeStorage({
+				key: 'searchList'
+			});
+
+			this.searchHistoryList = [];
+		}
+	},
 	async onLoad() {
 		let list = await uni.getStorage({
 			key: 'searchList'
@@ -211,7 +213,7 @@ methods: {
 		if (list[1].data) {
 			this.searchHistoryList = JSON.parse(list[1].data);
 		}
-	 }
+	}
 };
 </script>
 
@@ -225,15 +227,15 @@ methods: {
 	box-sizing: border-box;
 	padding: 0px 15px;
 	// margin-top:35rpx;
-	margin-top:160rpx;
-	.search1{
+	margin-top: 160rpx;
+	.search1 {
 		display: flex;
 		align-items: center;
 		// border:1px solid red;
 		.searchInput {
 			background-color: #fff;
 			width: 220px;
-			height:80rpx;
+			height: 80rpx;
 			margin-left: 5px;
 			margin-top: 35rpx;
 			/* border:1px solid red; */
@@ -248,39 +250,37 @@ methods: {
 			border: 1px solid rgba(217, 212, 212, 100);
 		}
 	}
-	.searchQuxiao{
+	.searchQuxiao {
 		width: 80px;
-		height:80rpx;
+		height: 80rpx;
 		line-height: 80rpx;
 		text-align: center;
 		border-radius: 32rpx;
 		margin-left: 5px;
 		margin-top: 35rpx;
 		/* border:1px solid red; */
-		background-color: #4CB5F6;
-		color:#fff;
+		background-color: #4cb5f6;
+		color: #fff;
 	}
-	
 }
-
 
 .searchHistory {
 	width: 100%;
 	margin-top: 5px;
-	.searchHistory1{
+	.searchHistory1 {
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
 		box-sizing: border-box;
 		padding: 0px 5px;
 		// border: 1px solid red;
-		.lishiSearch_title{
-			font-weight: bold; 
+		.lishiSearch_title {
+			font-weight: bold;
 			font-size: 40rpx;
 			/* border:1px solid red; */
-			margin-top:30rpx;
-			margin-left:50rpx;
-			
+			margin-top: 30rpx;
+			margin-left: 50rpx;
+
 			width: 112px;
 			height: 41px;
 			color: rgba(16, 16, 16, 100);
@@ -288,22 +288,22 @@ methods: {
 			// text-align: left;
 			font-family: SourceHanSansSC-bold;
 		}
-		.lishiSearch_title1{
+		.lishiSearch_title1 {
 			width: 50rpx;
 			height: 50rpx;
 			// border:1px solid red;
-			margin-right:25rpx;
+			margin-right: 25rpx;
 			font-size: 24px;
-			image{
-				width:100%;
-				height:100%;
+			image {
+				width: 100%;
+				height: 100%;
 			}
 		}
 	}
-	.isShows{
+	.isShows {
 		display: flex;
 	}
-	.isNotShows{
+	.isNotShows {
 		display: none;
 	}
 	.searchHistoryItem {
@@ -312,7 +312,7 @@ methods: {
 		display: flex;
 		flex-wrap: wrap;
 		// border: 1px solid red;
-		.searchHistoryItem1{
+		.searchHistoryItem1 {
 			height: 56rpx;
 			line-height: 56rpx;
 			text-align: center;
@@ -321,93 +321,234 @@ methods: {
 			background-color: #fff;
 			text-align: center;
 			border: 1px solid rgba(255, 255, 255, 100);
-			.searchHistoryItem_item{
+			.searchHistoryItem_item {
 				// width: 100%;
 				height: 29px;
 				color: rgba(16, 16, 16, 100);
 				font-size: 15px;
 				text-align: center;
-				color:#000;
+				color: #000;
 				// font-family: SourceHanSansSC-regular;
 			}
 		}
 	}
-	.content_contents{
-		width:100%;
-		height:auto;
+	.content_contents {
+		width: 100%;
+		height: auto;
 		margin-top: 50rpx;
 		// border:1px solid green;
-		.content_content{
-			width:100%;
-			height:150rpx;
+		.content_content {
+			width: 100%;
+			height: 150rpx;
 			// border:1px solid green;
-			margin-top:5rpx;
+			margin-top: 5rpx;
 			display: flex;
 			align-items: center;
 			justify-content: center;
-			.content_content1{
-				width:90%;
-				height:150rpx;
+			.content_content1 {
+				width: 90%;
+				height: 150rpx;
 				// border:1px solid pink;
-				border-bottom:1px solid #BBBBBB;
-				.content_content1_left{
-					width:70%;
-					height:150rpx;
+				border-bottom: 1px solid #bbbbbb;
+				.content_content1_left {
+					width: 70%;
+					height: 150rpx;
 					// border:1px solid red;
-					float:left;
-					.content_content1_left_top{
-						width:100%;
-						height:90rpx;
+					float: left;
+					.content_content1_left_top {
+						width: 100%;
+						height: 90rpx;
 						// border:1px solid green;
 						font-weight: bold;
 						// white-space: nowrap;
 						// word-break:break-all;
-						
+
 						overflow: hidden;
 						text-overflow: ellipsis;
 						display: -webkit-box;
 						-webkit-line-clamp: 2;
 						-webkit-box-orient: vertical;
 					}
-					.content_content1_left_bottom{
-						width:100%;
-						height:50rpx;
+					.content_content1_left_bottom {
+						width: 100%;
+						height: 50rpx;
 						// border:1px solid blue;
-						color:#9B9B9B;
-						font-size:20rpx;
-						.content_content1_left_bottom1{
-							width:150rpx;
-							height:50rpx;
-							line-height:50rpx;
+						color: #9b9b9b;
+						font-size: 20rpx;
+						.content_content1_left_bottom1 {
+							width: 150rpx;
+							height: 50rpx;
+							line-height: 50rpx;
 							text-align: left;
-							float:left;
+							float: left;
 							// border:1px solid red;
 						}
-						.content_content1_left_bottom2{
-							width:140rpx;
-							height:50rpx;
-							line-height:50rpx;
+						.content_content1_left_bottom2 {
+							width: 140rpx;
+							height: 50rpx;
+							line-height: 50rpx;
 							text-align: right;
-							float:right;
+							float: right;
 							// border:1px solid red;
 						}
 					}
 				}
-				.content_content1_right{
-					width:25%;
-					height:150rpx;
-					margin-left:10rpx;
+				.content_content1_right {
+					width: 25%;
+					height: 150rpx;
+					margin-left: 10rpx;
 					// border:1px solid green;
-					float:left;
-					image{
-						width:100%;
-						height:90%;
+					float: left;
+					image {
+						width: 100%;
+						height: 90%;
 					}
 				}
 			}
-		}	
+		}
 	}
 }
 
+.box {
+	width: 100%;
+	padding: 30rpx 40rpx;
+	background-color: #FFFFFF;
+}
 
+// 搜索框
+.search-box {
+	position: relative;
+	width: 670rpx;
+	height: 60rpx;
+	margin: 24rpx auto;
+
+	.sousuo {
+		position: absolute;
+		left: 28rpx;
+		top: 14rpx;
+		color: #ed5c4d;
+	}
+
+	input {
+		width: 520rpx;
+		height: 60rpx;
+		border: 2rpx solid #ed5c4d;
+		border-radius: 40rpx;
+		background-color: #ffffff;
+	}
+	.quxiao {
+		position: absolute;
+		right: 20rpx;
+		top: 0rpx;
+		width: 80rpx;
+		height: 60rpx;
+		font-size: 36rpx;
+		font-weight: 400;
+		line-height: 60rpx;
+		color: #a9afb8;
+	}
+}
+
+// 热搜
+.hot,.history{
+	width: 100%;
+	margin-top: 40rpx;
+}
+.title{
+	width: 100%;
+	height: 44rpx;
+	font-size: 16px;
+	font-weight: 500;
+	line-height: 44rpx;
+	color: #FFADA6;
+}
+.items{
+	display: flex;
+	flex-wrap: wrap;
+}
+.item{
+	height: 64rpx;
+	padding: 0rpx 30rpx;
+	margin: 30rpx 20rpx 0rpx 0rpx;
+	background: #F7F7F7;
+	text-align:center;
+	line-height: 64rpx;
+	border-radius: 60rpx;
+}
+// 清空样式
+.clear{
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 50%;
+	margin: 20rpx auto;
+	height: 60rpx;
+	font-size: 24rpx;
+	font-weight: 400;
+	color: #8A8A8A;
+	image{
+		width: 28rpx;
+		height: 28rpx;
+		margin-right: 10rpx;
+		
+	}
+}
+// 搜索内容样式
+
+.content-box{
+   width: 100%;
+   margin-top: 80rpx;
+}
+
+
+// 艺考动态列表样式
+		.dynamic-content,
+		.class-content {
+			display: flex;
+			align-items: center;
+			width: 670rpx;
+			height: 192rpx;
+			margin: 0rpx auto 28rpx;
+			border-radius: 16rpx;
+			background-color: #ffffff;
+		}
+		.dynamic-content .dynamic-img,
+		.class-content .class-img {
+			width: 232rpx;
+			height: 160rpx;
+			margin: 0rpx 16rpx;
+			border-radius: 8px;
+			overflow: hidden;
+		}
+		.dynamic-content .dynamic-img image,
+		.class-content .class-img image {
+			width: 100%;
+			height: 100%;
+		}
+		.dynamic-detail,
+		.class-detail {
+			width: 368rpx;
+			height: 160rpx;
+			display: flex;
+			flex-direction: column;
+		}
+		.dynamic-title,
+		.class-title {
+			width: 100%;
+			flex: 1;
+			font-size: 28rpx;
+			font-weight: 700;
+			line-height: 40rpx;
+			color: #0f1826;
+		}
+		.dynamic-pub {
+			display: flex;
+			justify-content: space-between;
+			width: 100%;
+			height: 34rpx;
+			font-size: 12px;
+			font-weight: 400;
+			line-height: 34rpx;
+			color: #a9afb8;
+		}
 </style>
